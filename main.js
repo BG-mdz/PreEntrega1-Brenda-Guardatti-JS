d6a0c2b462ee04a797f214d66f3a606d42eadfdc
BrendaG <brenmguarm@gmail.com>
2023-01-26 14:02:38 -0300

    se realizan cambios sobre switch y se agrega opcion 6

--- main.js
+++ main.js
@@ -1,69 +1,101 @@
-//productos
-const frambuesa = 300;
-const arandano = 120;
-const manzana = 1000;
-const vainilla = 400;
-let ingreseProducto;
-
-//Funciones
-//multiplicacion-FUNCTION
-
-function multiplicacion (item){
-    let ingreseCantidad;
-    let resultadoFinal;
-do{
-    ingreseCantidad = parseInt(prompt("Ingrese la cantidad de velas que desea asquirir (maximo 10)."));
-    resultadoFinal = item * ingreseCantidad;
-
-}while(ingreseCantidad<1 || ingreseCantidad>10 || isNaN(ingreseCantidad))
-    alert(`El precio final es: $${resultadoFinal}`)
+/**Objeto con CLASS */
+class Vela{
+    constructor (id,nombre,precio,oferta){
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.oferta = oferta;
+    }
 }
-//funcion COMPRA
-//productos-CICLOS
-function compra (){
-let ingreseProducto
+
+//ARRAY OBJETOS
+const velaFrambuesa = new Vela (1,"Frambuesa", 300,true);
+const velaArandano = new Vela (2, "Arandano", 120,false);
+const velaMAnzana = new Vela (3, "ManzanaCanela", 1000,false);
+const velaVainilla = new Vela (4, "Vainilla", 400,true);
+
+/**ARRAY de OBJETOS */
+const velas = [velaFrambuesa, velaArandano, velaMAnzana, velaVainilla];
+console.log (velas);
+
+const carrito =[];
+
+function seleccionProductos() {
+    let productoElegido;
+    do {
+        productoElegido = parseInt(prompt("Ingrese el numero del producto que desea adquirir:" + "\n" + "1. Vela de Frambuesa" + "\n" + "2. vela de Arandano"+ "\n"+ "3. Vela de Manzana & Canela" + "\n" + "4. Vela de Vainilla"  + "\n" +  "5. Continuar con la compra" +"\n" + "6. Para saber si el producto esta en oferta"+ "\n" + "0. Salir"))
+        if(productoElegido >= 0 && productoElegido <= 6){
+            switch (productoElegido) {
+                case 0:
+                    alert("Saliste, gracias")
+                    break;
+                case 5:
+                    pagarCarrito()
+                    break;
+                case 6:
+                    productoEnOferta()
+                    break;
+                default:
+                    buscarProductos(productoElegido)
+                    break;
+            }
+        } else {
+            alert("Ingresaste un valor inválido.")
+        }
+    } while (productoElegido !== 6 && productoElegido !== 0)
+}
+
+function productoEnOferta(){
+    let ingreseProducto;
     do{
-        ingreseProducto = parseInt(prompt("Ingrese el numero del producto que quiere adquirir: 1.frambuesa, 2. arandanos, 3. manzana, 4.vainilla, para salir ingrese 0"));
-    switch (ingreseProducto){
-        case 0:
-            alert("Gracias por su compra.")
-            break;
-        case 1:
-            multiplicacion (frambuesa);
-            break;
-        case 2:
-	        multiplicacion (arandano);
-            break;
-        case 3:
-	        multiplicacion (manzana);
-            break;
-        case 4:
-	        multiplicacion (vainilla);
-            break;
-        default:
-            alert ("Ingreso una opcion incorrecta")
-            break
+        ingreseProducto=Number(prompt("Ingrese producto del cual desea saber si esta en oferta"));
+        let velaBuscada = velas.find(vela=>vela.id===ingreseProducto);
+        if (velaBuscada.oferta){
+            alert("Este producto se encuentra en oferta");
+        }
+        else{
+            alert("Lo sentimoos. Actualmente este producto no esta en oferta");
+        }
+    }while(ingreseProducto !==0|| isNaN(ingreseProducto));
+}
+
+function buscarProductos(valor) {
+    const velaBuscada = velas.find(vela => vela.id === valor);
+    elegirCantidad(velaBuscada)
+}
+
+function elegirCantidad(velaElegida){
+    let cantidad = parseInt(prompt("Ingrese la cantidad deseada"));
+
+    if(carrito.some(vela => vela.id === velaElegida.id)){
+        carrito.forEach(vela => {
+            if(vela.id === velaElegida.id){
+                vela.cantidad = vela.cantidad + cantidad
+            }
+        })
+    } else {
+        let productoParaCarrito = {
+            id: velaElegida.id,
+            nombre: velaElegida.nombre,
+            precio: velaElegida.precio,
+            cantidad: cantidad,
+        }
+        carrito.push(productoParaCarrito)
+        alert("Producto agregado con éxito!")
     }
 
 }
-while (ingreseProducto !== 0)
-}
 
 
-//envios-CONDICIONALES
-let zona = prompt("Ingrese en que zona vive:" + "\n" + "Latinoamerica" + "\n" + "Europa" + "\n" +  "Asia ").toLowerCase();
+function pagarCarrito(){
 
+    let total = 0;
+    carrito.forEach(producto => {
+        total += producto.precio * producto.cantidad
+    })
 
-if (zona == "latinoamerica"){
-compra();
-}
-else if (zona == "europa"){
-    alert ("Consulte condiciones de envio");
-}
-else if (zona == "asia"){
-    alert ("No realizamos envios a su zona");
-}
-else{
-    alert ("Ha ingresado una zona no valida.");
+    alert("Felicidades. Pasá a pagar. El precio final es de: $" + total)
+    carrito = []
 }
 
+seleccionProductos();
