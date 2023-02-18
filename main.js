a0fba207f33a0025c447ef9b014d473a6365ae8e
BrendaG <brenmguarm@gmail.com>
2023-02-18 20:44:34 -0300

    se realizo fecth con productos

--- main.js
+++ main.js
@@ -1,138 +1,158 @@
-/**Objeto con CLASS */
-class Vela{
-    constructor (id,nombre,precio,oferta,img){
-        this.id = id;
-        this.nombre = nombre;
-        this.precio = precio;
-        this.oferta = oferta;
-        this.img = img;
-    }
+/**obtengo los productos con fetch dede JSON */
+fetch("productos.json")
+    .then((response)=>{
+        return response.json();
+    })
+    .then((responseProducts) => {
+        renderProducts (responseProducts);
+        showProducts();
+    })
+
+let products = [];
+let cart = [];
+
+/*CARGAR CARRITO DESDE EL LOCALSTORAGE*/
+//Si hay algo en el localStorage se carga en el carrito, si no es porque esta vacio.
+
+if(localStorage.getItem("cart")){
+    cart = JSON.parse(localStorage.getItem("cart"));
 }
+//DOM mostrando los productos
+const productsContainer = document.getElementById("productsContainer");
 
-//ARRAY OBJETOS
-const velaFrambuesa = new Vela (1,"Frambuesa", 300,true,"./assets/Frambuesa-500.png");
-const velaArandano = new Vela (2, "Arandano", 120,false, "img");
-const velaMAnzana = new Vela (3, "ManzanaCanela", 1000,false, "img");
-const velaVainilla = new Vela (4, "Vainilla", 400,true,"img");
+// función mostrar productos.
 
-/**ARRAY de OBJETOS */
-const velas = [velaFrambuesa, velaArandano, velaMAnzana, velaVainilla];
-console.log (velas);
-/**ARRAY carrito vacio */
-let carrito =[];
-/**MENU PRINCIPAL */
-function seleccionProductos() {
-    let productoElegido;
-    do {
-        productoElegido = parseInt(prompt("Ingrese el numero del producto que desea adquirir:" + "\n" + "1. Vela de Frambuesa" + "\n" + "2. vela de Arandano"+ "\n"+ "3. Vela de Manzana & Canela" + "\n" + "4. Vela de Vainilla"  + "\n" +  "5. Continuar con la compra" +"\n" + "6. Para saber si el producto esta en oferta"+ "\n" + "0. Salir"))
-        if(productoElegido >= 0 && productoElegido <= 6){
-            switch (productoElegido) {
-                case 0:
-                    alert("Saliste, gracias")
-                    break;
-                case 5:
-                    pagarCarrito()
-                    // let  total = pagarCarrito()
-                    // document.getElementByID ("total").innertext = `El total es: ${total}`;
-                    break;
-                case 6:
-                    productoEnOferta()
-                    break;
-                default:
-                    buscarProductos(productoElegido)
-                    break;
-            }
-        } else {
-            alert("Ingresaste un valor inválido.")
-        }
-    } while (productoElegido !== 6 && productoElegido !== 0)
+const renderProducts = (productsFetch) =>{
+    products = productsFetch
 }
+const showProducts = () => {
+    products.forEach(product => {
+        const card = document.createElement("div");
+        card.classList.add("col-xl-3", "col-md-6", "col-sm-12");
+        card.innerHTML = `
+                        <div class ="card">
+                            <img src = "${product.img}" class = "card-img-top imageProducts" alt = "${product.name}">
+                            <div>
+                                <h5> ${product.name} </h5>
+                                <p> ${product.price} </p>
+                                <button class = "btn colorBoton" id="button${product.id}" > Agregar al Carrito </button>
+                            </div>
+                        </div>
+                        `
+        productsContainer.appendChild(card);
 
-function buscarProductos(valor) {
-    const velaBuscada = velas.find(vela => vela.id === valor);
-    elegirCantidad(velaBuscada)
-}
-
-function elegirCantidad(velaElegida){
-    let cantidad = parseInt(prompt("Ingrese la cantidad deseada"));
-
-    if(carrito.some(vela => vela.id === velaElegida.id)){
-        carrito.forEach(vela => {
-            if(vela.id === velaElegida.id){
-                vela.cantidad = vela.cantidad + cantidad
-            }
+        //Agregar productos al carrito:
+        const button = document.getElementById(`button${product.id}`);
+        button.addEventListener("click", () => {
+            addToCart(product.id);
         })
+    })
+}
+
+showProducts();
+
+const addToCart = (id) => {
+    const productInCart = cart.find(product => product.id === id);
+    if(productInCart) {
+        productInCart.quantity += 1;
     } else {
-        let productoParaCarrito = {
-            id: velaElegida.id,
-            nombre: velaElegida.nombre,
-            precio: velaElegida.precio,
-            cantidad: cantidad,
-        }
-        carrito.push(productoParaCarrito)
-        alert("Producto agregado con éxito!")
+        const product = products.find(product => product.id === id);
+
+        createProductForCart(product.id, product.name, product.price, product.img)
+        swal({
+            title: "Genial!",
+            text: "Tu producto se agrego con exito al carrito",
+            icon: "success"});
     }
-
+    totalAmount();
+    localStorage.setItem("cart", JSON.stringify(cart));
 }
 
-function pagarCarrito(){
+const createProductForCart = (id, name, price, img) => {
+    let newProduct = {
+        id,
+        name,
+        price,
+        quantity: 1,
+        img
+    }
+    cart.push (newProduct);
+}
 
-    let total = 0;
-    carrito.forEach(producto => {
-        total += producto.precio * producto.cantidad
+//Mostrar el carrito de compras:
+
+const cartContainer = document.getElementById("cartContainer");
+const show = document.getElementById("showCart");
+
+show.addEventListener("click", () => {
+    showCart();
+})
+
+const showCart = () => {
+    cartContainer.innerHTML = "";
+    cart.forEach(product => {
+        const card = document.createElement("div");
+        card.classList.add("col-xl-3", "col-md-6", "col-sm-12");
+        card.innerHTML = `
+                        <div class ="card">
+                            <img src = "${product.img}" class = "card-img-top imgProductos" alt = "${product.name}">
+                            <div>
+                                <h5> ${product.name} </h5>
+                                <p> ${product.price} </p>
+                                <p> ${product.quantity} </p>
+                                <button class = "btn colorBoton" id="remove${product.id}" > Eliminar </button>
+                            </div>
+                        </div>
+                        `
+        cartContainer.appendChild(card);
+
+        //Eliminamos productos desde el carrito:
+        const button = document.getElementById(`remove${product.id}`);
+        button.addEventListener("click", () => {
+            removeFromCart(product.id);
+        })
     })
-    // agregarItemHTML(producto);
-    alert("Felicidades. Pasá a pagar. El precio final es de: $" + total)
-    carrito = []
+    totalAmount();
 }
 
-function productoEnOferta(){
-    let ingreseProducto;
-    do{
-        ingreseProducto=Number(prompt("Ingrese producto del cual desea saber si esta en oferta"));
-        let velaBuscada = velas.find(vela=>vela.id===ingreseProducto);
-        if (velaBuscada.oferta){
-            alert("Este producto se encuentra en oferta");
-        }
-        else{
-            alert("Lo sentimos. Actualmente este producto no esta en oferta");
-        }
-    }while(ingreseProducto !==0|| isNaN(ingreseProducto));
+//Funcion que elimina el producto del carrito:
+
+const removeFromCart = (id) => {
+    const product = cart.find(product => product.id === id);
+    const index = cart.indexOf(product);
+    cart.splice(index, 1);
+    swal({
+        title:"OK!",
+        text: "El producto fue eliminado",
+        icon: "success"});
+    showCart();
+
+    localStorage.setItem("cart", JSON.stringify(cart));
 }
-seleccionProductos();
 
+//Mostramos el total de la compra:
 
-//capturando eventos en el DOM
-let html = "";
-velas.forEach(element =>{
-    const classCard = element.oferta ? "card": "redCard"
-    html += //sugar syntax +=
-    `<div class= ${classCard}>
-        <li>
-            ${element.nombre}
-        </li>
-        <li>
-            ${element.precio}
-        </li>
-        <li>
-            ${element.oferta}
-        </li>
-        <button id= ${element.id} class="button" onclick=AddToCart(${element.id})>Agregar al carrito</button>
-        <button id= ${element.id} class="button" onclick=TakeOutfromCart(${element.id})>Remover del carrito</button>
-    </div>`
+const total = document.getElementById("total");
+
+const totalAmount = () => {
+    let totalPurchase = 0;
+    cart.forEach(product => {
+        totalPurchase += product.price * product.quantity;
     })
-document.getElementById("display-container").innerHTML = html;
-
-function AddToCart (elegirCantidad){
-    let producto = velas.find(element=>element.id===elegirCantidad)
-    carrito.push(producto)
-    localStorage.setItem("compra", JSON.stringify(carrito))
-    console.log(carrito);
+    total.innerHTML = `Total: $${totalPurchase}`;
 }
-// function TakeOutfromCart(removerCarrito){
 
-//     console.log();
-// }
+//Vaciar todo el carrito:
 
+const emptyCart = document.getElementById("emptyCart");
 
+emptyCart.addEventListener("click", () => {
+    emptyAllItemsInCart();
+})
 
+const emptyAllItemsInCart = () => {
+    cart = [];
+    showCart();
+
+    localStorage.clear();
+}
